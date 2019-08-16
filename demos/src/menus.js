86fd6735be7948ebd1c53d51aa1d160a81bf6891
itshizhan <leeson8888@itshizhan-MacBook-Pro.local>
2019-08-16 15:18:02 +0800

    add select component

--- demos/src/menus.js
+++ demos/src/menus.js
@@ -2,6 +2,7 @@
 import InputDemo from './views/input.vue'
 import RadioDemo from './views/raido'
 import CheckboxDemo from './views/checkbox'
+import SelectDemo from './views/select'
 // 以下为本地测试页面，testViews目录
 import SlotDemo from './testViews/SlotDemo.vue'
 import InputDemos from './testViews/inputdemos.vue'
@@ -17,6 +18,9 @@ let menus = [
   {
     name:'CheckboxDemo',
     component:CheckboxDemo,
+  },{
+    name:'SelectDemo',
+    component:SelectDemo,
   },
 ]
 let menusTest = [
---
+++ demos/src/views/select.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <o-select>Select Demos </o-select>
+  </div>
+</template>
+<script>
+export default {
+  name:'SelectDemo'
+}
+</script>
