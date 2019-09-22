783c2dfdaa2946b3a64febf5278e358ba928a4b3
itshizhan <leeson8888@itshizhan-MacBook-Pro.local>
2019-09-22 14:52:04 +0800

    add demos

--- demos/src/views/switch.vue
+++ demos/src/views/switch.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <o-switch @change="switchChanged" v-model="switchModel" >
+    <o-switch @change="switchChanged" v-model="switchModel"  :disabled="disabled">
     </o-switch>
-    <o-switch @change="switchChanged" v-model="switchModel" >
+    <o-switch @change="switchChanged" v-model="switchModel" :disabled="disabled">
       <template v-slot:open>是</template>
       <template v-slot:close>否</template>
     </o-switch>
-    <o-switch @change="switchChanged" v-model="switchModel" >
+    <o-switch @change="switchChanged" v-model="switchModel" :disabled="disabled">
       <template v-slot:open> <o-icon type="check" color="#fff"/> </template>
       <template v-slot:close> <o-icon type="x" color="grey"/> </template>
     </o-switch>
+
+    <p>
+      <button style="padding:2px 6px;margin-top:20px" @click="disabled=!disabled">disabled</button>
+    </p>
   </div>
 </template>
 <script>
@@ -18,10 +22,7 @@ export default {
   data(){
     return {
       switchModel:false,
-      slots:{
-        open:'开',
-        close:'关'
-      }
+      disabled:false
     }
   },
   methods:{
