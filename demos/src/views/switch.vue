6f7096bbbc8504d0f26fa83ccd0f37581d7f3a0f
itshizhan <leeson8888@itshizhan-MacBook-Pro.local>
2019-09-23 20:30:32 +0800

    update demos

--- demos/src/views/switch.vue
+++ demos/src/views/switch.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <o-switch @change="switchChanged" v-model="switchModel"  :disabled="disabled">
-    </o-switch>
+    <o-switch @change="switchChanged" v-model="switchModel" :disabled="disabled"></o-switch>
     <o-switch @change="switchChanged" v-model="switchModel" :disabled="disabled">
       <template v-slot:open>是</template>
       <template v-slot:close>否</template>
     </o-switch>
     <o-switch @change="switchChanged" v-model="switchModel" :disabled="disabled">
-      <template v-slot:open> <o-icon type="check" color="#fff"/> </template>
-      <template v-slot:close> <o-icon type="x" color="grey"/> </template>
+      <template v-slot:open>
+        <o-icon type="check" color="#fff" />
+      </template>
+      <template v-slot:close>
+        <o-icon type="x" color="grey" />
+      </template>
     </o-switch>
-
     <p>
       <button style="padding:2px 6px;margin-top:20px" @click="disabled=!disabled">disabled</button>
     </p>
@@ -18,17 +20,17 @@
 </template>
 <script>
 export default {
-  name:'SwitchDemo',
-  data(){
+  name: 'SwitchDemo',
+  data() {
     return {
-      switchModel:false,
-      disabled:false
+      switchModel: false,
+      disabled: false,
     }
   },
-  methods:{
-    switchChanged(val){
-      console.log("demos switchChanged",val)
-    }
-  }
+  methods: {
+    switchChanged(val) {
+      console.log('demos switchChanged', val)
+    },
+  },
 }
 </script>
