74aa35fc0ee7e12596ccc0870b65d671e01c2e87
itshizhan <leeson8888@itshizhan-MacBook-Pro.local>
2019-09-25 22:53:48 +0800

    perfect rate

--- demos/src/views/rate.vue
+++ demos/src/views/rate.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <o-rate icon-size="20"></o-rate>
+    <o-rate icon-size="20" v-model="curStar" @change="starChanged"></o-rate>
   </div>
 </template>
 <script>
 export default {
-  name:'RateDemo'
+  name: 'RateDemo',
+  data() {
+    return {
+      curStar: 3,
+    }
+  },
+  methods: {
+    starChanged(val) {
+      console.log('demos->onchange:', val)
+    },
+  },
 }
 </script>
--- packages/components/rate/ORate.vue
+++ packages/components/rate/ORate.vue
@@ -1,21 +1,25 @@
 <template>
   <div  class="oov-rate oov-rate-custom">
     <ul class="rate-list" @mouseover="mouseoverHandle" @mouseleave="mouseleaveHandle">
-      <li v-for="(item,index) of count" :key="index">
+      <li v-for="(item,index) of count" :key="index"  @click="rateConfirm(index)"  @mousemove="moveStarHandle(index, $event)">
         <o-icon :type="icon" color="#f5a623" :size="iconSize"></o-icon>
       </li>
     </ul>
-    <span class="rate-text">
-      <slot></slot>
-    </span>
   </div>
 </template>
 <script>
 export default {
   name:'ORate',
-  mode:{
+  model:{
     prop:'stars',
     event: 'select'
+  },
+  data(){
+    return {
+      currentValue:this.stars,
+      isHalf: this.allowHalf,
+      hoverIndex: -1,
+    }
   },
   props: {
     //selected stars，v-model bind
@@ -38,10 +42,6 @@ export default {
       type: Boolean,
       default: false
     },
-    showText: {
-      type: Boolean,
-      default: false
-    },
     icon: {
       type: String,
       default: 'star'
@@ -57,7 +57,34 @@ export default {
     },
     mouseleaveHandle(){
 
+    },
+    moveStarHandle(index, event){
+      if (this.disabled) return
+      this.hoverIndex = index
+      if (this.allowHalf && event.target.getAttribute('type') === 'half') {
+        this.isHalf = true
+      } else {
+        this.isHalf = false
+      }
+      // emit hover-change event
+      const exactlyHoverIndex = this.isHalf ? index - 0.5 : index
+      if (exactlyHoverIndex !== this.lastHoverIndex) {
+        this.$emit('on-hover-change', exactlyHoverIndex)
+      }
+      this.lastHoverIndex = exactlyHoverIndex
+    },
+    rateConfirm(index){
+      console.log("rateConfirm");
+      if (this.disabled) return
+      this.currentValue = this.isHalf ? index - 0.5 : index
+      console.log("rateConfirm",this.currentValue)
+      this.$emit('change', this.currentValue)
+      //v-model
+      this.$emit('select', this.currentValue)
     }
+  },
+  mounted(){
+    console.log("currentStar:",this.currentValue);
   }
 }
 </script>
